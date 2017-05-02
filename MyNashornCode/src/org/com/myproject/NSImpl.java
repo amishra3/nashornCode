a1134480e6cb22bd6c8e47eed63c781e25373861
ckj0369 <ckj0369@gmail.com>
2017-05-02 11:22:37 +0530

    Update NSImpl.java

--- MyNashornCode/src/org/com/myproject/NSImpl.java
+++ MyNashornCode/src/org/com/myproject/NSImpl.java
@@ -1,5 +1,5 @@
 package org.com.myproject;
-import java.io.InputStream;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
